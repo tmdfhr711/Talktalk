2d326a4e40db83d523e6e57e48fcd522685cb118
David.OH <dev_david@naver.com>
2018-02-13 01:06:18 +0900

    6. Create PeopleFragment
    1) 친구목록 불러오기

--- app/src/main/java/com/plplim/david/talktalk/MainActivity.java
+++ app/src/main/java/com/plplim/david/talktalk/MainActivity.java
@@ -3,11 +3,16 @@ package com.plplim.david.talktalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.plplim.david.talktalk.fragment.PeopleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new PeopleFragment()).commit();
+
     }
 }
---
+++ app/src/main/java/com/plplim/david/talktalk/fragment/PeopleFragment.java
@@ -0,0 +1,100 @@
+package com.plplim.david.talktalk.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.plplim.david.talktalk.R;
+import com.plplim.david.talktalk.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by OHRok on 2018-02-12.
+ */
+
+public class PeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_people, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.peoplefragment_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+
+        return view;
+    }
+
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+        List<UserModel> userModels;
+        public PeopleFragmentRecyclerViewAdapter() {
+            userModels = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    userModels.clear();
+                    //서버에서 넘어온 데이터
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        userModels.add(snapshot.getValue(UserModel.class));
+                    }
+                    //데이터가 쌓이고 난 뒤 새로고침
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend, parent, false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            Glide.with(holder.itemView.getContext())
+                    .load(userModels.get(position).profileImageUrl)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(((CustomViewHolder)holder).imageView);
+
+            ((CustomViewHolder) holder).textView.setText(userModels.get(position).userName);
+        }
+
+        @Override
+        public int getItemCount() {
+            return userModels.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView;
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = (ImageView) view.findViewById(R.id.frienditem_imageview);
+                textView = (TextView) view.findViewById(R.id.frienditem_textview);
+            }
+        }
+    }
+
+}
