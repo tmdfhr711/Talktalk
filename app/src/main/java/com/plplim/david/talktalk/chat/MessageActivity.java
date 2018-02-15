e10d40702e40a8e14a28813461925984b893a57f
David.OH <dev_david@naver.com>
2018-02-16 01:23:22 +0900

    10. 채팅방 데이터베이스 만들기
    1) ChatModel 수정
    2) 메세지 보내기 구현(데이터베이스 push)

--- app/src/main/java/com/plplim/david/talktalk/chat/MessageActivity.java
+++ app/src/main/java/com/plplim/david/talktalk/chat/MessageActivity.java
@@ -7,7 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.plplim.david.talktalk.R;
 import com.plplim.david.talktalk.model.ChatModel;
 
@@ -17,12 +20,16 @@ public class MessageActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
 
+    private String uid;
+    private String chatRoomUid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        destinationUid = getIntent().getStringExtra("destinationUid");
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid(); //채팅을 요구하는 아이디 즉 단말기에 로그인된 UID
+        destinationUid = getIntent().getStringExtra("destinationUid"); //채팅을 당하는 아이디
         button = (Button) findViewById(R.id.messageActivity_button);
         editText = (EditText) findViewById(R.id.messageActivity_editText);
 
@@ -30,10 +37,43 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ChatModel chatModel = new ChatModel();
-                chatModel.uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                chatModel.destinationUid = destinationUid;
+                chatModel.users.put(uid, true);
+                chatModel.users.put(destinationUid, true);
 
-                FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel);
+                if (chatRoomUid == null) {
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel);
+                } else {
+                    ChatModel.Comment comment = new ChatModel.Comment();
+                    comment.uid = uid;
+                    comment.message = editText.getText().toString();
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").push().setValue(comment);
+                }
+
+            }
+        });
+        checkChatRoom();
+    }
+
+    void checkChatRoom(){
+        //내가 속해있는 chatroom을 체크하는 함수
+
+        //oderByChild user 밑에 해당 아이디가 있는지 체크
+        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/" + uid).equalTo(true)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    ChatModel chatModel = item.getValue(ChatModel.class);
+                    //내가 요구한 chatrooms에 상대방의 아이디가 있는지 확인함
+                    if (chatModel.users.containsKey(destinationUid)) {
+                        chatRoomUid = item.getKey();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
--- app/src/main/java/com/plplim/david/talktalk/model/ChatModel.java
+++ app/src/main/java/com/plplim/david/talktalk/model/ChatModel.java
@@ -1,10 +1,19 @@
 package com.plplim.david.talktalk.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by OHRok on 2018-02-15.
  */
 
 public class ChatModel {
-    public String uid;
-    public String destinationUid;
+
+    public Map<String, Boolean> users = new HashMap<>(); //채팅방 유저들
+    public Map<String, Comment> comments = new HashMap<>(); //채팅방의 내용
+
+    public static class Comment {
+        public String uid;
+        public String message;
+    }
 }
