f9d4641b0fb0f3e2eeae0eeca7d1b32b86de5f9c
David.OH <dev_david@naver.com>
2018-02-19 22:13:49 +0900

    14. 대화방 리스트(Chat Room List) 만들기
    1) ChatFragment 생성
    2) fragment layout 생성
    3) BottomNavigationView 클릭 리스너 연결

--- app/src/main/java/com/plplim/david/talktalk/MainActivity.java
+++ app/src/main/java/com/plplim/david/talktalk/MainActivity.java
@@ -1,8 +1,13 @@
 package com.plplim.david.talktalk;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.plplim.david.talktalk.fragment.ChatFragment;
 import com.plplim.david.talktalk.fragment.PeopleFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new PeopleFragment()).commit();
+        BottomNavigationView bottomNavigationItemView = (BottomNavigationView) findViewById(R.id.mainactivity_bottomnavigationview);
+
+        bottomNavigationItemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_people:
+                        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new PeopleFragment()).commit();
+                        return true;
+                    case R.id.action_chat:
+                        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new ChatFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
 
     }
 }
